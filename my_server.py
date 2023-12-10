7b4982160e855ed0a1c05b9502df1e2ee2e7ea9e
dangtrantrung <trung280584@gmail.com>
2023-12-10 15:58:20 +0700

    OpenCV - Count face number

--- my_server.py
+++ my_server.py
@@ -2,11 +2,40 @@ from flask import request
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
+import numpy as np
+import cv2
+import base64
+
+
 # Khởi tạo Flask Server BE
 app=Flask(__name__)
 # Apply Flask - CORS
 CORS(app)
 app.config['CORS_HEADERS'] ='Content-Type'
+
+# hàm chuyển đổi từ base64 to image
+def from_base64_to_image(staff_pix):
+    try:
+        staff_pix=np.fromstring(base64.b64decode(staff_pix),dtype=np.uint8)
+        staff_pix=cv2.imdecode(staff_pix,cv2.IMREAD_ANYCOLOR)
+    except:
+        return None
+    return staff_pix
+
+# hàm đếm số mặt trong ảnh
+def dem_so_mat(face):
+
+     # Khởi tạo bộ phát hiện khuôn mặt
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    # Chuyen gray
+    gray = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+    # Phát hiện khuôn mặt trong ảnh
+    faces = face_cascade.detectMultiScale(gray, 1.2, 10)
+
+    so_mat = len(faces)
+    return so_mat
+
+
 
 # define App routes
 @app.route("/")
@@ -46,6 +75,25 @@ def upper_process():
 def upper_post_process():
     s=request.form.get('inputchuoi')
     return s.upper()
+
+@app.route('/nhandienkhuonmat',methods=['POST'])
+@cross_origin(origin='*')
+def nhandienkhuonmat_process():
+    face_number=0
+
+    # đọc ảnh từ client gửi lên
+    facebase64= request.form.get('facebase64')
+
+    # chuyển base64 về OpenCV format
+    face=from_base64_to_image(facebase64)
+
+    # đếm số mặt trong ảnh
+    face_number=dem_so_mat(face)
+
+    # trả về
+
+    return "Số mặt trong ảnh là: " +str(face_number)
+
 
 #Start Back End
 
